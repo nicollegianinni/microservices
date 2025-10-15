31e33a0344e5b4e6bc8026589121e0b3ce9c8d85
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-10-14 22:33:42 -0300

    init project

---
+++ usuario/src/test/java/com/ms/usuario/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
